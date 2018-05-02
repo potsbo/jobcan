2a13241629e6302438c712f8d07e45a02ef6369b
Shimpei Otsubo <potsbo@gmail.com>
2018-05-03 00:36:16 +0900

    Fix admin to return error

--- account/admin.go
+++ account/admin.go
@@ -1,11 +1,11 @@
 package account
 
 import (
-	"log"
 	"net/url"
 	"regexp"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/pkg/errors"
 )
 
 func (u *admin) Login() error {
@@ -17,41 +17,49 @@ func (u *admin) Login() error {
 	values.Add("url", "https://ssl.jobcan.jp/client/")
 	res, err := u.httpClient.PostForm("https://ssl.jobcan.jp/login/client", values)
 	if err != nil {
-		log.Fatal(err)
+		return errors.Wrap(err, "failed to post to login form")
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatal("Login error StatusCode=" + string(res.StatusCode))
+		return errors.Wrap(err, "Login error StatusCode="+string(res.StatusCode))
 	}
 	u.employeeLogin()
+	return nil
 }
 
-func (u *admin) employeeLogin() {
-	code := u.fetchEmployeeCode()
+func (u *admin) employeeLogin() error {
+	code, err := u.fetchEmployeeCode()
+	if err != nil {
+		return errors.Wrap(err, "failed to get employee code")
+	}
 	res, err := u.httpClient.Get("https://ssl.jobcan.jp/login/pc-employee/try?code=" + code)
 	if err != nil {
-		log.Fatal(err)
+		return errors.Wrap(err, "failed to login as an employee")
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatal("Login error StatusCode=" + string(res.StatusCode))
+		return errors.Wrap(err, "Login error StatusCode="+string(res.StatusCode))
 	}
+	return nil
 }
 
-func (u *admin) fetchEmployeeCode() string {
+func (u *admin) fetchEmployeeCode() (string, error) {
 	res, err := u.httpClient.Get("https://ssl.jobcan.jp/client")
 	if err != nil {
-		log.Fatal(err)
+		return "", errors.Wrap(err, "failed to get client page")
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatal("Login error StatusCode=" + string(res.StatusCode))
+		return "", errors.Wrap(err, "Login error StatusCode="+string(res.StatusCode))
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to create a new document")
+	}
 	attr, _ := doc.Find("#rollover-menu > li:nth-child(2)").Attr("onclick")
 	str := []byte(attr)
 	assigned := regexp.MustCompile("code=([0-9a-f]+)")
 	group := assigned.FindSubmatch(str)
-	return string(group[1])
+	return string(group[1]), nil
 }
